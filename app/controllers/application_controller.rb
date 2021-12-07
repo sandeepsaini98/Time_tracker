cf243bfea2c39c958b1d6a294ccd117a34e312c0
sandeepsaini98 <sandeepsaini85113@gmail.com>
2021-12-07 11:15:28 +0530

    Restricted Navigation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  helper_method :logged_in?
+
+
+  def logged_in?
+    !!current_user
+  end
 
   protected
 
